2fef86955e99b15ec8861d63917757b2c3c4094e
Santiago <santiifonzalida@gmail.com>
2024-08-06 11:02:40 -0300

    Metadata SEO

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function Page() {
     return <p>This is the customer page</p>
 }
